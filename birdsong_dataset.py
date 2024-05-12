e2cd9089bdfeb896a6e35d972b65b08c898b5c95
bpk_e <emrekarablt53@gmail.com>
2024-05-12 21:36:16 +0200

    TODO: apply noise reduction to Mel-Spectrogram

--- birdsong_dataset.py
+++ birdsong_dataset.py
@@ -5,12 +5,17 @@ from torch.utils.data import Dataset
 from pathlib import Path
 from sklearn.preprocessing import LabelEncoder
 import librosa
+import noisereduce as nr
+from scipy.io import wavfile
 
-#
+counter = 0
 class AudioDataset(Dataset):
-    def __init__(self, path = "data/songs", metadata_path= "data/birdsong_metadata.csv" , transform = None):
-        self.transform =  transform
+    def __init__(self, path = "data/songs", metadata_path= "data/birdsong_metadata.csv" , transform = None , max_len = 2, overlapping = 0.0, noise_reduction = False):
 
+        self.transform =  transform
+        self.max_len = max_len
+        self.overlapping = overlapping
+        self.noise_reduction = noise_reduction
         self.path = Path.cwd().joinpath(path)
         self.metadata = pd.read_csv(Path.cwd().joinpath(metadata_path))
         self.names = self.encode_names()
@@ -18,7 +23,7 @@ class AudioDataset(Dataset):
         self.data = self.load_data()
 
     def __len__(self):
-        return len(self.metadata)
+        return len(self.data)
 
     def encode_names(self):
         self.name_encoder = LabelEncoder()
@@ -37,25 +42,35 @@ class AudioDataset(Dataset):
         return self.species_encoder.inverse_transform(id)[0]
 
     def load_data(self):
-        data = []
+        assert (self.overlapping >= 0  and self.overlapping < 1 ), "invalid input"
+        samples = []
         print("Loading The Data...")
-        for idx in range(len(self)):
+        for idx in range(len(self.metadata)):
             file_name = "xc" + str(self.metadata.iloc[idx].file_id) + ".flac"
             audio_path = self.path.joinpath(file_name)
             audio, hz = librosa.load(audio_path)
-            mel_spec = self.convert_to_mel_spectrogram(audio,hz )
-            sample  = [np.array(mel_spec).reshape((mel_spec.shape[0],mel_spec.shape[1],1)),  np.array(self.names[idx]), np.array(self.species[idx])]
+            if self.noise_reduction:
+                audio = self.reduce_noise(audio,hz)
+            start = 0
+            while start + self.max_len * hz < len(audio):
+                sub_audio = audio[start : start + self.max_len * hz]
+                mel_spec = self.convert_to_mel_spectrogram(sub_audio,hz)
+                sample = [np.array(mel_spec).reshape((mel_spec.shape[0], mel_spec.shape[1], 1)), np.array(self.names[idx]), np.array(self.species[idx])]
+                start += int(self.max_len * hz // (1 - self.overlapping))
+                samples.append(sample)
+
+        for i in range(len(samples)):
             if self.transform is not None:
-                sample = self.transform(sample)
-            data.append(sample)
+                samples[i] = self.transform(samples[i])
         print("Data Loaded !")
 
-        return data
+        return samples
 
     def __getitem__(self, idx):
         return self.data[idx][0], self.data[idx][1], self.data[idx][2]
     def convert_to_mel_spectrogram(self, audio , hz):
         sgram = librosa.stft(audio)
+
         sgram_mag, _ = librosa.magphase(sgram)
         mel_scale_sgram = librosa.feature.melspectrogram(S=sgram_mag, sr=hz)
         mel_sgram = librosa.amplitude_to_db(mel_scale_sgram, ref=np.min)
@@ -63,6 +78,16 @@ class AudioDataset(Dataset):
     @staticmethod
     def visualize_melspectrogram(sample):
         librosa.display.specshow(sample[0], sr=sample[1], x_axis='time', y_axis='mel', hop_length=512)
+
+    def reduce_noise(self, audio , sr , name = None):
+
+        global counter
+        print("NOISE REDUCTICTON ")
+        reduced_noise = nr.reduce_noise(y=audio, sr=sr, prop_decrease = 0.5)
+        if name is not None:
+            wavfile.write(f"data/reduced/{name}.wav", sr, reduced_noise)
+            counter += 1
+        return reduced_noise
 
 #
 class ToTensor(object):
@@ -81,5 +106,5 @@ class ToTensor(object):
         image = image.transpose((2, 0, 1))
         return torch.from_numpy(image) , torch.from_numpy(name) , torch.from_numpy(species)
 
-ds = AudioDataset(transform=ToTensor())
-# print(ds[0])
+# ds = AudioDataset(transform=ToTensor() , max_len=3, overlapping=0, noise_reduction=True)
+# print(len(ds))
